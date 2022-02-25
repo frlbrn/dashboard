58c8d6adaf6b356be0376dbf99cdf2f466ae1e47
frlbrn <100315725+frlbrn@users.noreply.github.com>
2022-02-25 20:51:07 +0100

    Update ViewEnvironment.js

--- src/views/ViewEnvironment.js
+++ src/views/ViewEnvironment.js
@@ -9,6 +9,9 @@ import {
   Row,
   Col,
 } from "reactstrap";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet"
 
 const options = {
   responsive: true,
@@ -47,10 +50,26 @@ const options = {
     },
   },
 }
+  
+function get_date_to_display(key)
+{
+  const date_to_display = new Date(key);
+  let currentLocaleDate = date_to_display.toLocaleString('fr-FR',{
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'});
+  return currentLocaleDate
+}
 
 function ViewEnvironement() {
   const [electromagnetic_wave_evolution, setElectromagneticWaveEvolution] = useState([]);
 
+
+  let icon = L.icon({
+    iconSize:   [25, 41],
+    iconAnchor: [12, 41],
+    iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png"
+  });
   /*
   https://opendata.bordeaux-metropole.fr/explore/dataset/rt_ondeelectro_p_histo_jour/api/?disjunctive.ident&q=14781bc3-782e-4e12-a25c-52772c125f26&rows=10
   https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=rt_ondeelectro_p_histo_jour&q=14781bc3-782e-4e12-a25c-52772c125f26&rows=10&facet=time&facet=nom&facet=etat&facet=ident
@@ -69,7 +88,7 @@ function ViewEnvironement() {
         //data.push(['Jour', 'Ondes'])
         result.records.forEach(element => {
             //data.push([new Date(element.fields.time.split('-').join('/')), element.fields.mesure])
-            labels.unshift(element.fields.time)
+            labels.unshift(get_date_to_display(element.fields.time))
             values.unshift(element.fields.mesure)
         });
 
@@ -103,12 +122,33 @@ function ViewEnvironement() {
                 <CardTitle tag="h5">Ondes Electromagnétiques</CardTitle>
               </CardHeader>
               <CardBody>
+                <Row>
+              <Col lg="9" md="9" sm="12">
                 <Line
                   data={electromagnetic_wave_evolution}
                   options={options}
                   width={400}
                   height={150}
                 />
+              </Col>
+              <Col lg="3" md="3" sm="12">
+                <span>Localisation du capteur</span>
+                <MapContainer
+          style={{ height: "250px", width: "100%" }}
+          zoom={14}
+          center={[44.84238, -0.77900]}
+        >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'  />
+      <Marker icon={icon} position={[44.84238, -0.77900]} >
+          <Popup >
+          <img alt="capteur" width="300" src="https://storage.googleapis.com/odo-prod-sites/site_14781bc3-782e-4e12-a25c-52772c125f26/photo_Martignas_sur_Jalle_1000x1000.webp"/><br/><br/>
+          <span><a target="top" href="https://www.observatoiredesondes.com/fr/carte-des-ondes/capteurs/martignas-sur-jalle/14781bc3-782e-4e12-a25c-52772c125f26">Plus d'info...</a></span><br/>
+          </Popup>
+      </Marker>
+          </MapContainer>
+              </Col>
+              </Row>
               </CardBody>
               <CardFooter>
                 Sources:
