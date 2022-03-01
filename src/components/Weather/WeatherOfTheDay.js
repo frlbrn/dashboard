bdb40f1b48f0ea5aed4d97e8a116c3ab31684430
frlbrn <100315725+frlbrn@users.noreply.github.com>
2022-03-01 10:22:33 +0100

    Update WeatherOfTheDay.js

--- src/components/Weather/WeatherOfTheDay.js
+++ src/components/Weather/WeatherOfTheDay.js
@@ -23,6 +23,8 @@ const WeatherOfTheDay = props => {
             let month = currentDate.getMonth() + 1;
             if (month < 10)
                 month = '0'+month
+            if (date < 10)
+                date = '0'+date
             let year = currentDate.getFullYear();
             currentDate = year+"-"+month+"-"+date
 
